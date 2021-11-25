a83f67f717437a59982ff9a2cbb6ab57d925d920
Kushal Khattri <kushalk62@gmail.com>
2021-11-24 18:24:58 -0800

    added local storage and fixed color

--- script.js
+++ script.js
@@ -9,8 +9,13 @@ $(".row").each(function(){
         $(this).addClass('past');
     } else if (hour < time ){
         $(this).addClass('future');
+        $(this).addClass('past');
+        $(this).addClass('present');
     }else if (hour === time){
         $(this).addClass('present');
+        $(this).addClass('past');
+        $(this).addClass('present');
+     
     }
 
 });
@@ -21,8 +26,19 @@ $(".saveBtn").each(function(){
 });
 
 function savelocalstorages(event){
-    var btn = event.target;
-    btn.siblings(".description").val()
-
-    
+    var btn = $(event.target);
+    var text = btn.siblings(".description").val();
+    console.log(text);
+    var id = btn.parent().attr('id');
+    localStorage.setItem(id, text);
 }
+
+$('#9').children('.description').val(localStorage.getItem('9'));
+$('#10').children('.description').val(localStorage.getItem('10'));
+$('#11').children('.description').val(localStorage.getItem('11'));
+$('#12').children('.description').val(localStorage.getItem('12'));
+$('#13').children('.description').val(localStorage.getItem('13'));
+$('#14').children('.description').val(localStorage.getItem('14'));
+$('#15').children('.description').val(localStorage.getItem('15'));
+$('#16').children('.description').val(localStorage.getItem('16'));
+$('#17').children('.description').val(localStorage.getItem('17'));
